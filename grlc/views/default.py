b46311d844d42426cccf4f6dd46645a653d5660d
indepndnt <joecarey001@gmail.com>
2018-02-22 18:00:58 -0600

    a response

--- grlc/views/default.py
+++ grlc/views/default.py
@@ -3,5 +3,15 @@ from pyramid.view import view_config
 
 
 @view_config(route_name='home')
-def my_view(request):
-    return Response(200)
+def home_view(request):
+    return Response(418)
+
+
+@view_config(route_name='request', renderer='json')
+def request_view(request):
+    return {'uuid': 'unique-id-for-transaction', 'grlc_amt_str': 'exactly 12.345 GRLC', 'pmt_address': 'Garlic1234'}
+
+
+@view_config(route_name='status', renderer='json')
+def status_view(request):
+    return {'status': 'waiting', 'uuid': 'uuid'}
